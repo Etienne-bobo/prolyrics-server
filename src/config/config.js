736c77aa5ddd4e704b95584f37aa334b1536fb62
Etienne-bobo <etiennebobo17@gmail.com>
2020-04-07 08:38:41 +0530

    deploy4

--- src/config/config.js
+++ src/config/config.js
@@ -3,12 +3,12 @@ const path = require('path')
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'heroku_74ac03ad03149df',
-    user: process.env.DB_USER || 'be4b274904f8c4',
-    password: process.env.DB_PASS || 'f3760626',
+    database: 'heroku_74ac03ad03149df',
+    user: 'be4b274904f8c4',
+    password: 'f3760626',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'us-cdbr-iron-east-01.cleardb.net',
+      host: 'us-cdbr-iron-east-01.cleardb.net',
       storage: path.resolve(__dirname, '../../tabtracker.mysql')
     }
   },
